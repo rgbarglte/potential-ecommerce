7bf4fe4a3555a1e0117408ee994fdd17efefc528
Raa GB <46008942+rgbarglte@users.noreply.github.com>
2023-05-08 06:12:42 -0300

    Create meta.py

---
+++ lib/meta.py
@@ -0,0 +1,34 @@
+metas_seo = [
+    {
+        'platform': 'Facebook',
+        'meta_tags': [
+            {'property': 'og:title', 'content': 'Título de la meta para Facebook'},
+            {'property': 'og:description', 'content': 'Descripción de la meta para Facebook'},
+            {'property': 'og:image', 'content': 'URL de la imagen de la meta para Facebook'}
+        ]
+    },
+    {
+        'platform': 'Instagram',
+        'meta_tags': [
+            {'property': 'og:title', 'content': 'Título de la meta para Instagram'},
+            {'property': 'og:description', 'content': 'Descripción de la meta para Instagram'},
+            {'property': 'og:image', 'content': 'URL de la imagen de la meta para Instagram'}
+        ]
+    },
+    {
+        'platform': 'WhatsApp',
+        'meta_tags': [
+            {'property': 'og:title', 'content': 'Título de la meta para WhatsApp'},
+            {'property': 'og:description', 'content': 'Descripción de la meta para WhatsApp'},
+            {'property': 'og:image', 'content': 'URL de la imagen de la meta para WhatsApp'}
+        ]
+    },
+    {
+        'platform': 'Twitter',
+        'meta_tags': [
+            {'name': 'twitter:title', 'content': 'Título de la meta para Twitter'},
+            {'name': 'twitter:description', 'content': 'Descripción de la meta para Twitter'},
+            {'name': 'twitter:image', 'content': 'URL de la imagen de la meta para Twitter'}
+        ]
+    }
+]
